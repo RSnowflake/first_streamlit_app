d3c5de9283e50bf7590621f88d60daa5329317a9
RSnowflake <126164855+RSnowflake@users.noreply.github.com>
2023-02-24 09:19:49 +0530

    Update streamlit_app.py
    added 2 headers and few items under breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents new healthy Diner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('oatmeal')
 streamlit.text('Smoothie')
 streamlit.text('Boiled eggs')
+
+streamlit.header('Build your own fruit smoothie')
