aae92b9cf92cefef3ec93aca02ad8932f4fba9b9
RSnowflake <126164855+RSnowflake@users.noreply.github.com>
2023-02-24 09:16:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('oatmeal')
+streamlit.text('Smoothie')
+streamlit.text('Boiled eggs')
